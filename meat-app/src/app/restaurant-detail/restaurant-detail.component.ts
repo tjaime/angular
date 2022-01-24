7b0b12b2270c9037c0cb64206d53282283c79cb2
tjaime <thiagojaime@hotmail.com>
2022-01-23 23:50:35 -0300

    Paginas adicionais de detalhe e menu filho

--- meat-app/src/app/restaurant-detail/restaurant-detail.component.ts
+++ meat-app/src/app/restaurant-detail/restaurant-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { RestaurantsService } from '../restaurants/restaurants.service';
+import { Restaurant } from '../restaurants/restaurant/restaurant.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'mt-restaurant-detail',
@@ -6,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurantDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private restaurantsService: RestaurantsService, private route: ActivatedRoute) { }
+
+  restaurant!: Restaurant
 
   ngOnInit(): void {
+
+    this.restaurantsService
+      .getRestaurantById(this.route.snapshot.params['id'])
+      .subscribe(restaurant => this.restaurant = restaurant);
   }
+  
 
 }
