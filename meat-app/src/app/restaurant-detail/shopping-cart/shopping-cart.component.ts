3759b6d1b45fc7488fef9900606b85c6c4578b57
tjaime <thiagojaime@hotmail.com>
2022-01-24 01:23:07 -0300

    Template do total de itens do carrinho

--- meat-app/src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
+++ meat-app/src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ShoppingCartService } from './shopping-cart.service';
+
 
 @Component({
   selector: 'mt-shopping-cart',
@@ -6,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingCartComponent implements OnInit {
 
-  constructor() { }
+  constructor(private shoppingCartService: ShoppingCartService) { }
 
   ngOnInit(): void {
+  }
+
+  itens(): any {
+    return this.shoppingCartService.itens;
+  }
+
+  total(): number {
+    return this.shoppingCartService.total();
   }
 
 }
