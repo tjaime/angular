af089787b2bfafd87e59d7d955bc798fb4a65b8a
tjaime <thiagojaime@hotmail.com>
2022-01-24 01:39:20 -0300

    Carrinho de compras implementado parcialmente

--- meat-app/src/app/restaurant-detail/menu/menu.component.ts
+++ meat-app/src/app/restaurant-detail/menu/menu.component.ts
@@ -19,8 +19,4 @@ export class MenuComponent implements OnInit {
     this.menu = this.restaurantsService.getMenuOfRestaurant(this.route.parent?.snapshot.params['id'])
   }
 
-  addMenuItem(item: MenuItem) {
-    console.log(item)
-  }
-
 }
--- meat-app/src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
+++ meat-app/src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ShoppingCartService } from './shopping-cart.service';
-
+import { CartItem } from './cart-item.model';
+import { MenuItem } from '../menu-item/menu-item.model';
 
 @Component({
   selector: 'mt-shopping-cart',
@@ -20,5 +21,19 @@ export class ShoppingCartComponent implements OnInit {
   total(): number {
     return this.shoppingCartService.total();
   }
+
+  clear() {
+    this.shoppingCartService.clear()
+  }
+
+  removeItem(item: CartItem) {
+    this.shoppingCartService.removeItem(item)
+  }
+
+
+  addItem(item: MenuItem) {
+    this.shoppingCartService.addItem(item)
+  }
+
 
 }
